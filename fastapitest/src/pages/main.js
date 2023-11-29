7bcfabdfc91c684fb09c73150417471ec7873e3d
LSH <121619411+Choguesung@users.noreply.github.com>
2023-11-29 16:30:48 +0900

    1129

--- fastapitest/src/pages/main.js
+++ fastapitest/src/pages/main.js
@@ -8,14 +8,15 @@ function MyComponent() {
   const [content, setContent] = useState('');
   const [data, setData] = useState(null);
 
+  const [name, setName] = useState('');
+  const [result, setResult] = useState('');
+
   useEffect(() => {
     // 외부 서버에서 선수 데이터를 가져오는 부분입니다.
     axios.get('http://15.164.46.201/title').then((response) => {
       setData(response.data); // 선수 데이터를 상태에 저장
     });
   }, []);
-
-  console.log(data)
 
   const handleCreatePost = async () => {
     try {
@@ -25,7 +26,20 @@ function MyComponent() {
         content: content,
       });
 
-      console.log('Response:', response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error creating post:', error);
+    }
+  };
+
+  const handleRiotPost = async () => {
+    try {
+      // 요청 바디에 데이터를 담아 POST 요청 보내기
+      const response = await axios.post(`http://15.164.46.201/riotpost/${name}`);
+
+      setResult(response.data);
+
+      console.log(result);
     } catch (error) {
       console.error('Error creating post:', error);
     }
@@ -52,8 +66,21 @@ function MyComponent() {
         <br />
         <button onClick={handleCreatePost}>Create Post</button>
       </div>
+
+      <div>
+        <h2>닉네임 입력하셈</h2>
+        <label>닉네임:</label>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <br />
+        <button onClick={handleRiotPost}>전적검색</button>
+      </div>
+      <ul>
+      {Object.keys(result).map((key) => (
+          <p key={key}>{result[key].toString()}</p>
+        ))}
+      </ul>
     </div>
-  );
+  );  
 }
 
 export default MyComponent;
